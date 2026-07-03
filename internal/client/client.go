463db88084f16b9668a12575ed27f1aed627aa6d
Jean Machuca <correojean@gmail.com>
2026-07-03 21:15:02 +0000

    fix: unchecked rand.Read return value

--- internal/client/client.go
+++ internal/client/client.go
@@ -87,7 +87,7 @@ func (c *Conn) Send(env Envelope) error {
 
 func uuidV4() string {
 	b := make([]byte, 16)
-	rand.Read(b)
+	_, _ = rand.Read(b)
 	b[6] = (b[6] & 0x0f) | 0x40
 	b[8] = (b[8] & 0x3f) | 0x80
 	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x",
