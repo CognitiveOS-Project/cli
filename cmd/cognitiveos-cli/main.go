b69f0dc88730a36c3a88b2701b7f283c3f454d90
Jean Machuca <correojean@gmail.com>
2026-07-09 03:44:45 +0000

    feat: Phase 6 distro build pipeline + CLI daemon spawning

--- cmd/cognitiveos-cli/main.go
+++ cmd/cognitiveos-cli/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/CognitiveOS-Project/cli/internal/client"
 	"github.com/CognitiveOS-Project/cli/internal/tui"
@@ -16,6 +18,28 @@ func main() {
 	flag.Parse()
 
 	conn := client.New(*socketPath)
+
+	if _, err := os.Stat(*socketPath); os.IsNotExist(err) {
+		cmd := exec.Command("cognitiveosd")
+		cmd.Stderr = os.Stderr
+		if err := cmd.Start(); err != nil {
+			log.Fatalf("spawn cognitiveosd: %v", err)
+		}
+		defer func() {
+			cmd.Process.Signal(os.Interrupt)
+			cmd.Wait()
+		}()
+		for i := 0; i < 8; i++ {
+			if _, err := os.Stat(*socketPath); err == nil {
+				break
+			}
+			time.Sleep(250 * time.Millisecond)
+		}
+	}
+
+	if err := conn.Connect(); err != nil {
+		log.Fatalf("connect: %v", err)
+	}
 
 	p := tea.NewProgram(
 		tui.NewModel(conn),
